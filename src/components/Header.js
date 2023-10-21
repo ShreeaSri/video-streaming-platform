2ea66c6e5dfc57953942f6d0c7178054c2ac4336
Shreea Sri <shreeasri@gmail.com>
2023-10-21 08:51:19 +0530

    built toggle feature of hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+
   return (
-    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+    <div className="grid grid-flow-col p-3 m-1">
       <div className="flex col-span-1">
         <img
-          className="h-10"
+          onClick={() => toggleMenuHandler()}
+          className="h-10 cursor-pointer"
           alt="hamburger-menu"
           src="https://static.vecteezy.com/system/resources/previews/021/190/402/original/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg"
         />
-        <img
-          className="h-10 mx-2"
-          alt="youtube-logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
-        />
+        <a href="/">
+          <img
+            className="h-10 mx-2"
+            alt="youtube-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          />
+        </a>
       </div>
       <div className="col-span-10 text-center px-10">
         <input
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  if (!isMenuOpen) return null;
+  return (
+    <div className="pt-2 pl-6 w-48">
+      <ul className="border-b">
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Videos</li>
+        <li className="pb-2">Live</li>
+      </ul>
+      <h1 className="font-bold pt-2">Subscription</h1>
+      <ul className="border-b">
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li className="pb-2">Movies</li>
+      </ul>
+      <h1 className="font-bold pt-2">Watch Later</h1>
+      <ul className="border-b">
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li className="pb-2">Movies</li>
+      </ul>
+      <h1 className="font-bold pt-2">Explore</h1>
+      <ul className="border-b">
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li className="pb-2">Movies</li>
+      </ul>
+      <h1 className="font-bold pt-2">Library</h1>
+      <ul className="border-b">
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li className="pb-2">Movies</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
